e91d322011f3775bdfb904623891c7ae25dd0e36
Ben <benjorourke1999@gmail.com>
2020-12-03 13:21:59 +0000

    Add homing & basic green object detection

--- scripts/behaviours.py
+++ scripts/behaviours.py
@@ -8,3 +8,6 @@ class Behaviour:
 
     def act(self, robot, sequencer):
         print("Error: child class should override this")
+
+    def object_of_interest_found(self, robot, sequencer):
+        print("Error")
